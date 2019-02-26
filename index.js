36d3ccf54171a28461b952c166c3476e0f11009a
Akashita <swan.silverlight@gmail.com>
2019-02-26 19:17:12 +0100

    Add button animation

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-function add_module(){
-  document.getElementById('add_module_menu').style.display = 'flex';
-  document.getElementById('main_page').style.filter = 'blur(3px)'
-}
+
+$("#button_add_module").click(function(){
+  $("#add_module_menu").css({"display" : 'flex'});
+  $("#add_module_menu").animate({opacity : '1'});
+});
+
+$("#close_module_window").click(function(){
+  $("#add_module_menu").animate({opacity : '0'});
+  setTimeout(function(){
+    $("#add_module_menu").css({"display" : 'none'});
+  }, 1000)
+})
+
+$("#settings_buttons").click(function(){
+  $("#settings_menu").css({"display" : 'flex'});
+  $("#settings_menu").animate({opacity : '1'});
+});
+
+$("#close_settings_window").click(function(){
+  $("#settings_menu").animate({opacity : '0'});
+  setTimeout(function(){
+    $("#settings_menu").css({"display" : 'none'});
+  }, 1000)
+})
